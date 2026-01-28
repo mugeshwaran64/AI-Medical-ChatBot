ba851f3119f5cc01e75b3be5182c83799f7e245e
mugeshwaran64 <mugeshwaran64@gmail.com>
2026-01-28 12:01:43 +0530

    "FILE SETUP COMPLETED"

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='AI-Medical-ChatBot',
+    version='0.1.0',
+    author='Mugeshwaran C',
+    author_email='cmugeshwaran30@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,22 @@
+# Creating directories
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch LICENSE
+touch README.md
+touch template.sh
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+# src files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+
+# research files
+touch research/trials.ipynb
+
+echo "Directory and files created successfully!"
